658fabd37b6728f85892eb005a3c092c0dc3d8d2
ikun <ikun0014@qq.com>
2024-07-19 23:19:42 +0800

    重开吧！！！

---
+++ LICENSE.md
@@ -0,0 +1,67 @@
+# Anti CSDN License (ACSL)
+
+Version 1.0, June 2024
+
+Copyright 2024
+
+Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+
+## Preamble
+
+The proliferation of software and the ease of copying and modifying it has led to a wide variety of licenses designed to protect the rights of creators while enabling collaboration and further development of the software. This license, the Anti CSDN License (ACSL), is designed with the explicit intent of prohibiting the CSDN and its related websites, including 'gitcode' (hereinafter referred to as "CSDN") from copying, modifying, or redistributing the software (hereinafter referred to as "the Software") it applies to, while still maintaining the software's status as open source for others. The ACSL aims to promote the free use, modification, and sharing of the Software by the open-source community, with the sole restriction of CSDN's involvement.
+
+## CSDN Details
+
+CSDN (Chinese Software Developer Network) belongs to Beijing Innovation Lezhi Network Technology Co., Ltd. It is a Chinese information technology knowledge service website with services including information technology dissemination and communication, education and training, and professional technical talent services. It operates a network community, learning platform, and communication platform.
+
+## Terms and Conditions
+
+### 1. Definitions
+
+"This License" refers to version 2.0 of the Anti CSDN License.
+
+"The Software" refers to the software distributed under this License.
+
+"CSDN" refers to the website and all its affiliated entities and services that are known for aggregating and redistributing content without explicit permission from the original creators. This includes but is not limited to Gitcode. For detailed information, CSDN (Chinese Software Developer Network) belongs to Beijing Innovation Lezhi Network Technology Co., Ltd. It is a Chinese information technology knowledge service website with services including information technology dissemination and communication, education and training, and professional technical talent services. It operates a network community, learning platform, and communication platform.
+
+### 2. Grant of Copyright License
+
+Subject to the terms and conditions of this License, each contributor to the Software grants you a worldwide, royalty-free, non-exclusive, perpetual copyright license to reproduce, prepare derivative works of, publicly display, publicly perform, sublicense, and distribute the Software and such derivative works in source code or object form.
+
+### 3. Prohibition for CSDN
+
+Notwithstanding the above grant, CSDN and its associated entities, including Gitcode, are expressly prohibited from:
+
+a. Copying, modifying, or redistributing the Software or any derivative works thereof, in any form.
+
+b. Using the Software for any form of aggregation, compilation, or database that is accessible on or through any platforms owned, operated, or controlled by CSDN and its associated entities, including Gitcode.
+
+c. Engaging in any activity that directly or indirectly infringes on the rights granted under this License to any user of the Software.
+
+### 4. Redistribution
+
+You may reproduce and distribute copies of the Software or any derivative works thereof in any medium, with or without modifications, provided that you meet the following conditions:
+
+a. You must give any other recipients of the Software or derivative works a copy of this License; and
+
+b. You must cause any modified files to carry prominent notices stating that you changed the files; and
+
+c. You must retain, in the Software or derivative works, all copyright and permission notices as found in the original Software.(To prevent theft, recommend that you also add Anti CSDN license for your projects)
+
+### 5. Disclaimer of Warranty
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+### 6. Limitation of Liability
+
+In no event and under no legal theory, whether in tort (including negligence), contract, or otherwise, unless required by applicable law (such as deliberate and grossly negligent acts) or agreed to in writing, shall any contributor to the Software be liable to you for damages, including any direct, indirect, special, incidental, or consequential damages of any character arising as a result of this License or out of the use or inability to use the Software (including but not limited to damages for loss of goodwill, work stoppage, computer failure or malfunction, or any and all other commercial damages or losses), even if such contributor has been advised of the possibility of such damages.
+
+### 7. Accepting Warranty or Additional Liability
+
+While redistributing the Software or derivative works thereof, you may choose to offer, and charge a fee for, acceptance of support, warranty, indemnity, or other liability obligations and/or rights consistent with this License. However, in accepting such obligations, you may act only on your own behalf and on your sole responsibility, not on behalf of any other contributors to the Software, and only if you agree to indemnify, defend, and hold each contributor harmless for any liability incurred by, or claims asserted against, such contributor by reason of your accepting any such warranty or additional liability.
+
+## END OF TERMS AND CONDITIONS
+
+By using, copying, modifying, or distributing the Software (or any work based on the Software), you agree to be bound by the terms of this License. If you do not agree to the terms of this License, do not use, copy, modify, or distribute the Software.
+
+If you seek to redistribute the Software in a manner not permitted by this License, or if you have questions about obtaining additional permissions, please contact the original creators of the Software.
---
+++ main.py
@@ -0,0 +1,312 @@
+import os
+import vdf
+import winreg
+import argparse
+import aiohttp
+import aiofiles
+import traceback
+import subprocess
+import colorlog
+import logging
+import json
+import time
+import sys
+import psutil
+import asyncio
+from pathlib import Path
+
+# 初始化日志记录器
+def init_log():
+    logger = logging.getLogger('Onekey')
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    fmt_string = '%(log_color)s[%(name)s][%(levelname)s]%(message)s'
+    log_colors = {
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'purple'
+    }
+    fmt = colorlog.ColoredFormatter(fmt_string, log_colors=log_colors)
+    stream_handler.setFormatter(fmt)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+# 生成配置文件
+def gen_config_file():
+    default_config = {"Github_Persoal_Token": "", "Custom_Steam_Path": ""}
+    with open('./config.json', 'w', encoding='utf-8') as f:
+        json.dump(default_config, f)
+    log.info('程序可能为第一次启动，请填写配置文件后重新启动程序')
+
+
+# 加载配置文件
+def load_config():
+    if not os.path.exists('./config.json'):
+        gen_config_file()
+        os.system('pause')
+        sys.exit()
+    else:
+        with open('./config.json', 'r', encoding='utf-8') as f:
+            config = json.load(f)
+            return config
+
+
+log = init_log()
+config = load_config()
+lock = asyncio.Lock()
+
+
+print('\033[1;32;40m  _____   __   _   _____   _   _    _____  __    __ \033[0m')
+print('\033[1;32;40m /  _  \ |  \ | | | ____| | | / /  | ____| \ \  / /\033[0m')
+print('\033[1;32;40m | | | | |   \| | | |__   | |/ /   | |__    \ \/ /\033[0m')
+print('\033[1;32;40m | | | | | |\   | |  __|  | |\ \   |  __|    \  /')
+print('\033[1;32;40m | |_| | | | \  | | |___  | | \ \  | |___    / /\033[0m')
+print('\033[1;32;40m \_____/ |_|  \_| |_____| |_|  \_\ |_____|  /_/\033[0m')
+log.info('作者ikun0014')
+log.info('本项目基于wxy1343/ManifestAutoUpdate进行修改，采用GPL V3许可证')
+log.info('版本：1.0.3')
+log.info('项目仓库：https://github.com/ikunshare/Onekey')
+log.debug('官网：ikunshare.com')
+log.warning('注意：据传Steam新版本对部分解锁工具进行了检测，但目前未发现问题，如果你被封号可以issue反馈')
+log.warning('本项目完全免费，如果你在淘宝，QQ群内通过购买方式获得，赶紧回去骂商家死全家\n交流群组：\n点击链接加入群聊【ikun分享】：https://qm.qq.com/q/D9Uiva3RVS\nhttps://t.me/ikunshare_group')
+
+
+# 通过注册表获取Steam安装路径
+def get_steam_path():
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Valve\Steam')
+    steam_path = Path(winreg.QueryValueEx(key, 'SteamPath')[0])
+    custom_steam_path = config.get("Custom_Steam_Path", "")
+    if not custom_steam_path == '':
+        return Path(custom_steam_path)
+    else:
+        return steam_path
+
+
+steam_path = get_steam_path()
+isGreenLuma = any((steam_path / dll).exists() for dll in ['GreenLuma_2024_x86.dll', 'GreenLuma_2024_x64.dll', 'User32.dll'])
+isSteamTools = (steam_path / 'config' / 'stplug-in').is_dir()
+
+
+# 错误堆栈处理
+def stack_error(exception):
+    stack_trace = traceback.format_exception(type(exception), exception, exception.__traceback__)
+    return ''.join(stack_trace)
+
+
+# 下载清单
+async def get(sha, path):
+    url_list = [
+        f'https://gcore.jsdelivr.net/gh/{repo}@{sha}/{path}',
+        f'https://fastly.jsdelivr.net/gh/{repo}@{sha}/{path}',
+        f'https://cdn.jsdelivr.net/gh/{repo}@{sha}/{path}',
+        f'https://github.moeyy.xyz/https://raw.githubusercontent.com/{repo}/{sha}/{path}',
+        f'https://mirror.ghproxy.com/https://raw.githubusercontent.com/{repo}/{sha}/{path}',
+        f'https://ghproxy.org/https://raw.githubusercontent.com/{repo}/{sha}/{path}',
+        f'https://raw.githubusercontent.com/{repo}/{sha}/{path}'
+    ]
+    retry = 3
+    async with aiohttp.ClientSession() as session:
+        while retry:
+            for url in url_list:
+                try:
+                    async with session.get(url) as r:
+                        if r.status == 200:
+                            return await r.read()
+                        else:
+                            log.error(f'获取失败: {path} - 状态码: {r.status}')
+                except aiohttp.ClientError:
+                    log.error(f'获取失败: {path} - 连接错误')
+            retry -= 1
+            log.warning(f'重试剩余次数: {retry} - {path}')
+    log.error(f'超过最大重试次数: {path}')
+    raise Exception(f'Failed to download: {path}')
+
+
+# 获取清单信息
+async def get_manifest(sha, path, steam_path: Path):
+    collected_depots = []
+    try:
+        if path.endswith('.manifest'):
+            depot_cache_path = steam_path / 'depotcache'
+            async with lock:
+                if not depot_cache_path.exists():
+                    depot_cache_path.mkdir(exist_ok=True)
+            save_path = depot_cache_path / path
+            if save_path.exists():
+                async with lock:
+                    log.warning(f'已存在清单: {path}')
+                return collected_depots
+            content = await get(sha, path)
+            async with lock:
+                log.info(f'清单下载成功: {path}')
+            async with aiofiles.open(save_path, 'wb') as f:
+                await f.write(content)
+        elif path == 'Key.vdf':
+            content = await get(sha, path)
+            async with lock:
+                log.info(f'密钥下载成功: {path}')
+            depots_config = vdf.loads(content.decode(encoding='utf-8'))
+            for depot_id, depot_info in depots_config['depots'].items():
+                collected_depots.append((depot_id, depot_info['DecryptionKey']))
+    except KeyboardInterrupt:
+        raise
+    except Exception as e:
+        log.error(f'处理失败: {path} - {stack_error(e)}')
+        traceback.print_exc()
+        raise
+    return collected_depots
+
+
+# 合并DecryptionKey
+async def depotkey_merge(config_path, depots_config):
+    if not config_path.exists():
+        async with lock:
+            log.error('Steam默认配置不存在，可能是没有登录账号')
+        return
+    with open(config_path, encoding='utf-8') as f:
+        config = vdf.load(f)
+    software = config['InstallConfigStore']['Software']
+    valve = software.get('Valve') or software.get('valve')
+    steam = valve.get('Steam') or valve.get('steam')
+    if 'depots' not in steam:
+        steam['depots'] = {}
+    steam['depots'].update(depots_config['depots'])
+    with open(config_path, 'w', encoding='utf-8') as f:
+        vdf.dump(config, f, pretty=True)
+    return True
+
+
+# 增加SteamTools解锁相关文件
+async def stool_add(depot_data, app_id):
+    lua_filename = f"Onekey_unlock_{app_id}.lua"
+    lua_filepath = steam_path / "config" / "stplug-in" / lua_filename
+
+    async with lock:
+        log.info(f'SteamTools解锁文件生成: {lua_filepath}')
+        with open(lua_filepath, "w", encoding="utf-8") as lua_file:
+            lua_file.write(f'addappid({app_id}, 1, "None")\n')
+            for depot_id, depot_key in depot_data:
+                lua_file.write(f'addappid({depot_id}, 1, "{depot_key}")\n')
+
+    luapacka_path = steam_path / "config" / "stplug-in" / "luapacka.exe"
+    subprocess.run([str(luapacka_path), str(lua_filepath)])
+    os.remove(lua_filepath)
+    return True
+
+
+# 增加GreenLuma解锁相关文件
+async def greenluma_add(depot_id_list):
+    app_list_path = steam_path / 'appcache' / 'appinfo.vdf'
+    if app_list_path.exists() and app_list_path.is_file():
+        app_list_path.unlink(missing_ok=True)
+    if not app_list_path.is_dir():
+        app_list_path.mkdir(parents=True, exist_ok=True)
+    depot_dict = {}
+    for i in app_list_path.iterdir():
+        if i.stem.isdecimal() and i.suffix == '.txt':
+            with i.open('r', encoding='utf-8') as f:
+                app_id_ = f.read().strip()
+                depot_dict[int(i.stem)] = None
+                if app_id_.isdecimal():
+                    depot_dict[int(i.stem)] = int(app_id_)
+    for depot_id in depot_id_list:
+        if int(depot_id) not in depot_dict.values():
+            index = max(depot_dict.keys()) + 1 if depot_dict.keys() else 0
+            if index != 0:
+                for i in range(max(depot_dict.keys())):
+                    if i not in depot_dict.keys():
+                        index = i
+                        break
+            with (app_list_path / f'{index}.txt').open('w', encoding='utf-8') as f:
+                f.write(str(depot_id))
+            depot_dict[index] = int(depot_id)
+    return True
+
+
+# 检测Github Api请求数量
+async def check_github_api_limit(headers):
+    url = 'https://api.github.com/rate_limit'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as r:
+            r_json = await r.json()
+            remain_limit = r_json['rate']['remaining']
+            use_limit = r_json['rate']['used']
+            reset_time = r_json['rate']['reset']
+            f_reset_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(reset_time))
+            log.info(f'已用Github请求数：{use_limit}')
+            log.info(f'剩余Github请求数：{remain_limit}')
+            if r.status == 429:
+                log.info(f'你的Github Api请求数已超限，请尝试增加Persoal Token')
+                log.info(f'请求数重置时间：{f_reset_time}')
+    return True
+
+
+# 检查进程是否运行
+def check_process_running(process_name):
+    for process in psutil.process_iter(['name']):
+        if process.info['name'] == process_name:
+            return True
+    return False
+
+
+# 主函数
+async def main(app_id):
+    app_id_list = list(filter(str.isdecimal, app_id.strip().split('-')))
+    app_id = app_id_list[0]
+    github_token = config.get("Github_Persoal_Token", "")
+    headers = {'Authorization': f'Bearer {github_token}'} if github_token else None
+
+    if headers:
+        await check_github_api_limit(headers)
+
+    url = f'https://api.github.com/repos/{repo}/branches/{app_id}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as r:
+            r_json = await r.json()
+            if 'commit' in r_json:
+                sha = r_json['commit']['sha']
+                url = r_json['commit']['commit']['tree']['url']
+                date = r_json['commit']['commit']['author']['date']
+                async with session.get(url, headers=headers) as r2:
+                    r2_json = await r2.json()
+                    if 'tree' in r2_json:
+                        collected_depots = []
+                        for i in r2_json['tree']:
+                            result = await get_manifest(sha, i['path'], steam_path)
+                            collected_depots.extend(result)
+                        if collected_depots:
+                            if isSteamTools:
+                                await stool_add(collected_depots, app_id)
+                                log.info('找到SteamTools，已添加解锁文件')
+                            if isGreenLuma:
+                                await greenluma_add([app_id])
+                                depot_config = {'depots': {depot_id: {'DecryptionKey': depot_key} for depot_id, depot_key in collected_depots}}
+                                depotkey_merge(steam_path / 'config' / 'config.vdf', depot_config)
+                                if await greenluma_add([int(i) for i in depot_config['depots'] if i.isdecimal()]):
+                                    log.info('找到GreenLuma，已添加解锁文件')
+                            log.info(f'清单最后更新时间：{date}')
+                            log.info(f'入库成功: {app_id}')
+                            return True
+    log.error(f'清单下载或生成.st失败: {app_id}')
+    return False
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--app-id')
+args = parser.parse_args()
+repo = 'ManifestHub/ManifestHub'
+if __name__ == '__main__':
+    try:
+        log.debug('App ID可以在SteamDB或Steam商店链接页面查看')
+        asyncio.run(main(args.app_id or input('需要入库的App ID: ')))
+    except KeyboardInterrupt:
+        exit()
+    except Exception as e:
+        log.error(f'发生错误: {stack_error(e)}')
+        traceback.print_exc()
+    if not args.app_id:
+        os.system('pause')
