b1b0fe95170fc46821eaed64ae0dcd833e972723
ikun <ikun0014@qq.com>
2024-09-16 13:45:21 +0800

    feat: 进度条

--- common/manifest_down.py
+++ common/manifest_down.py
@@ -1,5 +1,8 @@
 from aiohttp import ClientError
+from tqdm.asyncio import tqdm_asyncio
+
 from .log import log
+
 
 async def get(sha, path, repo, session):
     url_list = [
@@ -14,12 +17,21 @@ async def get(sha, path, repo, session):
             try:
                 async with session.get(url, ssl=False) as r:
                     if r.status == 200:
-                        return await r.read()
+                        total_size = int(r.headers.get('Content-Length', 0))
+                        chunk_size = 1024
+                        content = bytearray()
+
+                        with tqdm_asyncio(total=total_size, unit='B', unit_scale=True, desc=f'下载 {path}') as pbar:
+                            async for chunk in r.content.iter_chunked(chunk_size):
+                                content.extend(chunk)
+                                pbar.update(len(chunk))
+                        
+                        return content
                     else:
                         log.error(f' 🔄 获取失败: {path} - 状态码: {r.status}')
             except ClientError:
                 log.error(f' 🔄 获取失败: {path} - 连接错误')
         retry -= 1
-        log.warning(f'  🔄  重试剩余次数: {retry} - {path}')
-    log.error(f'  🔄 超过最大重试次数: {path}')
-    raise Exception(f'  🔄 无法下载: {path}')
+        log.warning(f' 🔄 重试剩余次数: {retry} - {path}')
+    log.error(f' 🔄 超过最大重试次数: {path}')
+    raise Exception(f' 🔄 无法下载: {path}')
