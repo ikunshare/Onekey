6fc06a681a1ebf6d5e4f2e4ab0cc2e5d29c80fa4
WangXianming <119822522+muwenyan521@users.noreply.github.com>
2024-10-12 20:19:00 +0800

    1.2.6

--- common/init_text.py
+++ common/init_text.py
@@ -18,7 +18,7 @@ def init():
     
     log.info('作者:ikun0014')
     log.info('本项目采用GNU General Public License v3开源许可证')
-    log.info('版本:1.2.5')
+    log.info('版本:1.2.6')
     log.info('项目仓库:https://github.com/ikunshare/Onekey')
     log.info('官网:ikunshare.com')
     log.warning('本项目完全开源免费,如果你在淘宝,QQ群内通过购买方式获得,赶紧回去骂商家死全家\n交流群组:\nhttps://qm.qq.com/q/d7sWovfAGI\nhttps://t.me/ikunshare_qun')
