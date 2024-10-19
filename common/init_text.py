fb0806aea70f4c150403e5b0c5594eb7eea10289
ikun0014 <ikun0014@qq.com>
2024-10-19 11:19:27 +0800

    [release] 在main分支版本下删除所有Emoji

--- common/init_text.py
+++ common/init_text.py
@@ -18,7 +18,7 @@ def init():
     
     log.info(f'作者: {Fore.RED}{Back.BLACK}{Style.BRIGHT}ikun0014{Style.RESET_ALL}')
     log.info(f'{Fore.RED}{Back.BLACK}{Style.BRIGHT}本项目采用GNU General Public License v3开源许可证，请勿用于商业用途{Style.RESET_ALL}')
-    log.info('版本: 1.2.9')
+    log.info('版本: 1.3.0')
     log.info(f'{Fore.RED}{Back.BLACK}{Style.BRIGHT}项目Github仓库: https://github.com/ikunshare/Onekey{Style.RESET_ALL}')
     log.info('官网: ikunshare.com')
     log.warning('本项目完全开源免费, 如果你在淘宝, QQ群内通过购买方式获得, 赶紧回去骂商家死全家\n交流群组:\n    https://t.me/ikunshare_qun')
